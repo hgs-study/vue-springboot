0d63ea9c24918232b8bdbcd70fa6e7a98da655a6
hgs-study <hgstudy_@naver.com>
2021-10-31 16:01:21 +0900

    리플렉션을 이용한 어노테이션 검색

---
+++ src/main/java/com/vuespringboot/business/post/reflection/TestClass.java
@@ -0,0 +1,12 @@
+package com.vuespringboot.business.post.reflection;
+
+public class TestClass {
+
+    @TestAnnotation(name = "Hyun", value = "1")
+    public void TestMethod(){
+    }
+
+    @TestAnnotation(name = "Lee", value = "2")
+    public void TestMethod2(){
+    }
+}
---
+++ src/main/java/com/vuespringboot/business/post/reflection/TestController.java
@@ -0,0 +1,26 @@
+package com.vuespringboot.business.post.reflection;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+
+@RestController
+public class TestController {
+
+    //리플렉션을 이용한 어노테이션 검색
+    @GetMapping("/reflection/test")
+    public void test() throws NoSuchMethodException {
+        Method method = TestClass.class.getMethod("TestMethod");
+        Annotation[] annotations = method.getDeclaredAnnotations();
+
+        for (Annotation annotation : annotations) {
+            if(annotation instanceof TestAnnotation){
+                TestAnnotation testAnnotation = (TestAnnotation) annotation;
+                System.out.println("testAnnotation.name() = " + testAnnotation.name());
+                System.out.println("testAnnotation.value() = " + testAnnotation.value());
+            }
+        }
+    }
+}
